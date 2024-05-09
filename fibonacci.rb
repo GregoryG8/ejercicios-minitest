c0c3889846d69c0b4ca8f24265d49d163b8cc5be
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-05-09 01:02:06 -0500

    Initial commit

---
+++ fibonacci.rb
@@ -0,0 +1,17 @@
+
+
+# fibonacci.rb
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144.
+
+
+class Fibonacci
+    def calculate(n)
+      return 1 if n <= 2
+      calculate(n - 1) + calculate(n - 2)
+    end
+  end
+  
+  20.times do |n|
+    puts Fibonacci.new.calculate(n)
+  end
+  
